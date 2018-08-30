1a8be55b0e5aaeca6d8ecfc10d6fdb18ec818436
ScatterBrain25 <mbattle25@live.com>
2018-08-30 14:29:28 -0600

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hello Everyone</h1>
+    <p>Welcome to my Gatsby site!.</p>
+    <p>I am new to programing but I am eager to learn and look forward to working
+      with everyone during this development process!
+    </p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
---
+++ src/pages/page-2.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const SecondPage = () => (
+  <div>
+    <h1>Welcome to the Second Page</h1>
+    <p>I don't have much to say at this moment but stay tuned for upcoming updates</p>
+    <Link to="/">Go back to the homepage</Link>
+  </div>
+)
+
+export default SecondPage
